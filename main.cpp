bd791b28cce278350fe5fd62c236c3c64bc12586
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2025-01-17 13:40:33 +0300

    Add main.cpp and CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "AVLTree.h"
+
+int main() {
+    AVLTree<int> tree;
+    tree.Insert(5);
+    tree.Insert(4);
+    tree.Insert(3);
+    tree.Insert(2);
+    tree.Insert(1);
+    tree.Insert(0);
+    return 0;
+}
